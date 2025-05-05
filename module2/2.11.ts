899b05cb269b295ea006cec0555d491048a2330e
sumonmia360 <sumonmia4526@gmail.com>
2025-05-05 22:45:04 +0600

    pick,omit,required,readonly,partial,record learing is done

---
+++ module2/2.11.ts
@@ -0,0 +1,48 @@
+{
+  /// uitily types
+
+  //pick type
+  type House = {
+    table: string;
+    chair: string;
+    desintable: string;
+    membar?: number;
+  };
+  type Table = Pick<House, "table" | "membar">;
+
+  type ChairDesintable = Pick<House, "chair" | "desintable">;
+  //omit means bad deya
+  type Membar = Omit<House, "membar" | "chair">;
+  type Table1 = Omit<House, "table">;
+
+  // Required
+
+  type AllRequired = Required<House>;
+  //partial
+  type Allpartial = Partial<House>;
+
+  // Readonly
+  type AllReadonly = Readonly<House>;
+
+  // Record
+
+  type RecordCheck = {
+    a: string;
+    b: string;
+  };
+  type RecordType = Record<string, string>;
+  const emtyObj: Record<string, unknown> = {
+    sumon: "sm",
+    mia: 54,
+    car: true,
+  };
+  const recordList: RecordType = {
+    a: "sumon",
+    b: "mia",
+    c: "muhammad",
+    d: "sumonmia",
+    emon: "miaemon",
+  };
+
+  ///
+}
